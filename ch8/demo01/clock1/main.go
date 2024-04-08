9e7b64a1e03bc2755ada7abb167edbd94fd33b5f
caowei <1840065113@qq.com>
2024-04-08 10:26:50 +0800

    完善8.1题

--- ch8/demo01/clock1/main.go
+++ ch8/demo01/clock1/main.go
@@ -1,40 +1,36 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"io"
 	"log"
 	"net"
 	"time"
 )
 
-// 支持输入传入参数作为端口号
-var port = flag.String("port", "8080", "请输入端口")
-
 func main() {
-	listener, err := net.Listen("tcp", "localhost:"+*port)
+	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("net.Dial failed, err = %v", err)
 	}
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Print(err) // e.g., connection aborted
+			log.Fatalf("accepet failed, err = %v", err)
 			continue
 		}
-		go handleConn(conn) // handle one connection at a time
-		fmt.Println("ping")
+		handler(conn)
+
 	}
+
 }
 
-func handleConn(c net.Conn) {
+func handler(c net.Conn) {
 	defer c.Close()
 	for {
 		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
 		if err != nil {
-			return // e.g., client disconnected
+			log.Fatalf("io.WriteString failed, err := %v", err)
+			return
 		}
 		time.Sleep(1 * time.Second)
 	}
